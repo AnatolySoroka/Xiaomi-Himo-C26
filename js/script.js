ed688a1afa16b418d01830a85ead951e2a57f5ea
AnatolySoroka <toliksoroka66@gmail.com>
2022-08-05 00:36:51 +0300

    created section offer

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('.slider__items').slick({
+        arrows: false,
+        dots: true,
+        // autoplay: true,
+        vertical: true,
+        verticalSwiping: true
+    });
+});
