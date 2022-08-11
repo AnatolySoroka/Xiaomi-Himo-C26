dc3e1527c171f2c6d8d5233b5adbd265fe5be4b6
AnatolySoroka <toliksoroka66@gmail.com>
2022-08-11 18:24:49 +0300

    created galery section

--- js/script.js
+++ js/script.js
@@ -6,4 +6,12 @@ $(function() {
         vertical: true,
         verticalSwiping: true
     });
+
+    $('.galery__slider-items').slick({
+        arrows: false,
+        dots: true,
+        // autoplay: true,
+        vertical: true,
+        verticalSwiping: true
+    });
 });
